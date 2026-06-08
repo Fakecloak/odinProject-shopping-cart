7aa050c827e1807a7e180aed50c053570f10272e
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-06-08 19:18:06 +0530

    created and setup react router

---
+++ src/Router/routes.jsx
@@ -0,0 +1,24 @@
+import Home from '../pages/Home.jsx';
+import Shop from '../pages/Shop.jsx';
+import Cart from '../pages/Cart.jsx';
+import ErrorPage from '../pages/ErrorPage.jsx';
+
+const routes = [
+  {
+    path : "/",
+    element : <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path : "/shop",
+    element : <Shop />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path : "/cart",
+    element : <Cart />,
+    errorElement: <ErrorPage />,
+  },
+];
+
+export default routes;
