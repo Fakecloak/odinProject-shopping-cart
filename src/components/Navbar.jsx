8a75d577deef820f285d5fa7af281dae93f71576
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-06-09 00:00:23 +0530

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,20 @@
 import { NavLink } from "react-router-dom";
 
+const NavLinkClass = ({isActive}) => isActive ? 'font-bold' : '';
+
 function Navbar () {
   return (
     <nav>
       <h1>Odin Shop Cart</h1>
 
       <div>
-        <NavLink to='/' className={
-            ({isActive}) => isActive ? 'font-bold' : ''
-        }>
+        <NavLink to='/' className={NavLinkClass}>
         Home
         </NavLink>
-        <NavLink to='/shop' className={
-            ({isActive}) => isActive ? 'font-bold' : ''
-        }>
+        <NavLink to='/shop' className={NavLinkClass}>
         Shop
         </NavLink> 
-        <NavLink to='/cart' className={
-            ({isActive}) => isActive ? 'font-bold' : ''
-        }>
+        <NavLink to='/cart' className={NavLinkClass}>
         Cart
         </NavLink>
       </div>
