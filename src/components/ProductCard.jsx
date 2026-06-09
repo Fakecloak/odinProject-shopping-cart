3718bd439ecb15404704d447cfa7cf6287a3579e
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-06-10 01:16:52 +0530

    Update: enhance ProductCard with quantity controls and add to cart functionality using useContext hook

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,15 @@
 import {useState} from "react";
+import {useContext} from "react";
+import {CartContext} from "../context/CartContext.jsx";
 
 function ProductCard( {product} ) {
     const [quantity, setQuantity] = useState(1);
+    const {addToCart} = useContext(CartContext);
+
+    function handleAddToCart() {
+        addToCart({...product, quantity});
+        setQuantity(1);
+    }
 
     return (
         <div className="product-card">
@@ -9,7 +17,7 @@ function ProductCard( {product} ) {
                 <img src={product.image} alt={product.title} />
             </div>
             <h2>{product.title}</h2>
-            <h3>{product.price}</h3>
+            <h3>$ {product.price.toFixed(2)}</h3>
             {/* <h4>{product.description}</h4> */}
             {/* <h4>{product.category}</h4> */}
             {/* <h4> { product.rating.rate} ( {product.rating.count} reviews )</h4> */}
@@ -26,6 +34,7 @@ function ProductCard( {product} ) {
                 >
                     +
                 </button>
+                <button onClick={handleAddToCart}>Add to Cart</button>
             </div>
         </div>
     )
