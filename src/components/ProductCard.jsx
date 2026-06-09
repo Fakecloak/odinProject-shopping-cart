ae5c83a67b5d847d0d50414e8b07a9af2b2a4fa6
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-06-10 00:57:52 +0530

    Update: add quantity controls inside ProductCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,15 +1,32 @@
+import {useState} from "react";
+
 function ProductCard( {product} ) {
+    const [quantity, setQuantity] = useState(1);
 
     return (
         <div className="product-card">
-        <div className="product-image">
-            <img src={product.image} alt={product.title} />
-        </div>
-        <h2>{product.title}</h2>
-        <h3>{product.price}</h3>
-        <h4>{product.description}</h4>
-        <h4>{product.category}</h4>
-        <h4> { product.rating.rate} ( {product.rating.count} reviews )</h4>
+            <div className="product-image">
+                <img src={product.image} alt={product.title} />
+            </div>
+            <h2>{product.title}</h2>
+            <h3>{product.price}</h3>
+            {/* <h4>{product.description}</h4> */}
+            {/* <h4>{product.category}</h4> */}
+            {/* <h4> { product.rating.rate} ( {product.rating.count} reviews )</h4> */}
+            <div className="quantity-controls">
+                <button
+                onClick={ () => setQuantity(prev => prev - 1)}
+                disabled={quantity === 1}
+                >
+                    -
+                </button>
+                <span> {quantity} </span>
+                <button
+                onClick={ () => setQuantity(prev => prev + 1)}
+                >
+                    +
+                </button>
+            </div>
         </div>
     )
 }
