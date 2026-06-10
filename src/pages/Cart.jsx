609f0b39bce3d848afd1b759f97661dc32a65b2d
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-06-11 03:41:07 +0530

    add CartItem Component to display Cart Items

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,13 +1,23 @@
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext.jsx";
+import CartItem from "../components/CartItem.jsx";
 
 function Cart() {
-  const { cart } = useContext(CartContext);
+  const { cart} = useContext(CartContext);
   // console.log(cart)
 
   return (
     <div className="cart">
       <h1>Cart</h1>
+
+      <div>
+        {cart.map(item => (
+          <CartItem 
+          key={item.id}
+          item={item}
+          />
+        ))}
+      </div>
     </div>
   );
 }
