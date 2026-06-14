3a79a8c4ba02771b4fef214d138051d1b15d8c8a
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-06-15 03:53:16 +0530

    fix case sensitive typo on import router

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from './router/routes.jsx';
+import routes from './Router/routes.jsx';
 import CartProvider from './context/CartContext.jsx';
 
 const router = createBrowserRouter(routes);
